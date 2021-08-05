f491bb3e6c86ce7b63ee2892ae65a3828558a7cd
kdaugh <kdaugherty@my365.bellevue.edu>
2021-08-05 12:09:44 -0500

    For simplicity's sake, I added format method to a couple of strings.

--- Week 9 Assignment.py
+++ Week 9 Assignment.py
@@ -7,13 +7,13 @@ def main():
         if os.path.isdir('{}'.format(question_2)):
             os.chdir(question_2)
             question_1 = input('\nWhat would you like to name the file? ')
-            logging.basicConfig(filename='{}.txt'.format(question_1), level=logging.INFO)
-            print('\nThis directory is valid...\nThe file "{}" has been created.'.format(question_1))
+            logging.basicConfig(filename='{}.log'.format(question_1), level=logging.INFO)
+            print('\nThis directory is valid...\nThe file "{}" has been saved.'.format(question_1))
             question_3 = input('\nWhat is your name? ')
             question_4 = input('What is your address? ')
             question_5 = input('Whats is your phone number? ')
-            logging.info(question_3 + ', ' + question_4 + ', ' + question_5)
-            print('\n'+ question_3 + ", " + question_4 + ", " + question_5)
+            logging.info('{}, {}, {}'.format(question_3, question_4, question_5))
+            print('\n{}, {}, {}'.format(question_3, question_4, question_5))
         else:
             print("\nDirectory does not exist.")
             main()
