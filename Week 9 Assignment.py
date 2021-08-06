c7a7d20fa39f38838537aa916ba5a3960ff12116
kdaugh <kdaugherty@my365.bellevue.edu>
2021-08-06 03:22:59 -0500

    Minor adjustment.

--- Week 9 Assignment.py
+++ Week 9 Assignment.py
@@ -6,9 +6,9 @@ def main():
         question_2 = input('\nPlease enter a directory path in which you would like to save the log file: ')
         if os.path.isdir('{}'.format(question_2)):
             os.chdir(question_2)
-            question_1 = input('\nWhat would you like to name the file? ')
+            question_1 = input('\nThis directory is valid...\nWhat would you like to name the file? ')
             logging.basicConfig(filename='{}.log'.format(question_1), level=logging.INFO)
-            print('\nThis directory is valid...\nThe file "{}" has been saved.'.format(question_1))
+            print('\nThe file "{}" has been saved.'.format(question_1))
             question_3 = input('\nWhat is your name? ')
             question_4 = input('What is your address? ')
             question_5 = input('Whats is your phone number? ')
